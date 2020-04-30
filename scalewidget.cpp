f70593fbcffff9035c272ca07e6860f0aae4886c
Demon-Cat <870603664@qq.com>
2020-04-30 18:00:36 +0800

    实现中心放大

--- scalewidget.cpp
+++ scalewidget.cpp
@@ -2,6 +2,7 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QWheelEvent>
+#include <QtDebug>
 
 ScaleWidget::ScaleWidget(QWidget *parent) : QWidget(parent)
 {
@@ -14,6 +15,14 @@ void ScaleWidget::paintEvent(QPaintEvent *event)
     painter.setPen(Qt::NoPen);
     painter.setBrush(QColor("#80FF0000"));
     painter.drawRect(rect());
+
+    painter.save();
+    painter.setPen(QPen(QColor("#FFFFFF"), 1));
+    for (int x = 0; x < width(); x += 10)
+    {
+        painter.drawLine(QPoint(x, 0), QPoint(x, height()));
+    }
+    painter.restore();
 }
 
 void ScaleWidget::mousePressEvent(QMouseEvent *event)
@@ -28,7 +37,24 @@ void ScaleWidget::mouseReleaseEvent(QMouseEvent *event)
 
 void ScaleWidget::mouseMoveEvent(QMouseEvent *event)
 {
-    move(event->globalPos() - m_pressDistance);
+    QPoint p = event->globalPos() - m_pressDistance;
+    if (p.x() > 0)
+    {
+        p.setX(0);
+    }
+    if (p.y() > 0)
+    {
+        p.setY(0);
+    }
+    if (p.x() + width() < parentWidget()->width())
+    {
+        p.setX(parentWidget()->width() - width());
+    }
+    if (p.y() + height() < parentWidget()->height())
+    {
+        p.setY(parentWidget()->height() - height());
+    }
+    move(p);
 }
 
 void ScaleWidget::wheelEvent(QWheelEvent *event)
@@ -37,7 +63,8 @@ void ScaleWidget::wheelEvent(QWheelEvent *event)
     QPoint numDegrees = event->angleDelta();
     if (!numDegrees.isNull())
     {
-        if (numDegrees.y() < 0)
+        qDebug() << numDegrees.y();
+        if (numDegrees.y() > 0)
         {
             if (m_zoom < 1000)
             {
@@ -52,5 +79,21 @@ void ScaleWidget::wheelEvent(QWheelEvent *event)
             }
         }
     }
+    qreal lastScale = m_scale;
     m_scale = m_zoom / 100.0;
+
+    if (lastScale == m_scale)
+    {
+        return;
+    }
+
+    QPoint p = mapToParent(mousePoint);
+    QTransform transform;
+    transform.translate(p.x(), p.y());
+    transform.scale(m_scale / lastScale, m_scale / lastScale);
+    transform.translate(-p.x(), -p.y());
+    QRect previousRc = geometry();
+    QRect nextRc = transform.mapRect(previousRc);
+
+    setGeometry(nextRc);
 }
